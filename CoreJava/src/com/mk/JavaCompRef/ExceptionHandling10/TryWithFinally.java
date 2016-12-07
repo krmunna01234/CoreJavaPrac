c4de61ae0a1fe08db15b0ab908853c9db3577805
krmunna01234 <krmunna01234@gmail.com>
2016-12-07 10:20:49 +0530

    Checkin

--- CoreJava/src/com/mk/JavaCompRef/ExceptionHandling10/TryWithFinally.java
+++ CoreJava/src/com/mk/JavaCompRef/ExceptionHandling10/TryWithFinally.java
@@ -1,14 +1,11 @@
 package com.mk.JavaCompRef.ExceptionHandling10;
 
-
-
 public class TryWithFinally {
-	public static void main(String as[]){
-		
-		try{
+	public static void main(String as[]) {
+
+		try {
 			System.out.println("Hello");
-		}
-		finally{
+		} finally {
 			System.out.println("finally Block");
 		}
 	}
